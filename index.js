8dc71db79c66d5427edbd4bd322bdf00215bcfb5
tommyHellraiser <nacho.ponce25@gmail.com>
2023-06-25 20:42:14 -0300

    Fixed mouse stuck problem checking viewport size in JS

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+$(".button").on("mousedown", function(e) {
+    $(this).addClass("button-active");
+});
+
+$(".button").on("mouseup", function(e) {
+    $(this).removeClass("button-active");
+});
+
 $(".button").on("touchstart", function(e) {
     $(this).addClass("button-active");
 });
@@ -6,18 +14,12 @@ $(".button").on("touchend", function(e) {
     $(this).removeClass("button-active button-hover");
 });
 
-$(".button").on("mouseover", function(e) {
-    $(this).addClass("button-hover");
-});
+if ($(window).width() > 768) {
+    $(".button").on("mouseover", function(e) {
+        $(this).addClass("button-hover");
+    });
 
-$(".button").on("mouseout", function(e) {
-    $(this).removeClass("button-hover");
-});
-
-$(".button").on("mousedown", function(e) {
-    $(this).addClass("button-active");
-});
-
-$(".button").on("mouseup", function(e) {
-    $(this).removeClass("button-active");
-});
+    $(".button").on("mouseout", function(e) {
+        $(this).removeClass("button-hover");
+    });
+}
