6b3ff86598f81382f2c66e05edee270403bc1151
tommyHellraiser <nacho.ponce25@gmail.com>
2023-06-25 20:45:56 -0300

    Collapsed several jsuqeries into a single one, and disabled user scalable design (no zoom allowed)

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 $(".button").on("mousedown", function(e) {
     $(this).addClass("button-active");
-});
-
-$(".button").on("mouseup", function(e) {
+}).on("mouseup", function(e) {
     $(this).removeClass("button-active");
-});
-
-$(".button").on("touchstart", function(e) {
+}).on(".button").on("touchstart", function(e) {
     $(this).addClass("button-active");
-});
-
-$(".button").on("touchend", function(e) {
+}).on("touchend", function(e) {
     $(this).removeClass("button-active button-hover");
 });
+
+// $(".button").on("mouseup", function(e) {
+//     $(this).removeClass("button-active");
+// });
+
+// $(".button").on("touchstart", function(e) {
+//     $(this).addClass("button-active");
+// });
+
+// $(".button").on("touchend", function(e) {
+//     $(this).removeClass("button-active button-hover");
+// });
 
 if ($(window).width() > 768) {
     $(".button").on("mouseover", function(e) {
         $(this).addClass("button-hover");
-    });
-
-    $(".button").on("mouseout", function(e) {
+    }).on("mouseout", function(e) {
         $(this).removeClass("button-hover");
     });
+
+    // $(".button").on("mouseout", function(e) {
+    //     $(this).removeClass("button-hover");
+    // });
 }
