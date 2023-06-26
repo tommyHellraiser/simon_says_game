4cc4ce1ec5dda2019322e44b3514675f43c07add
tommyHellraiser <nacho.ponce25@gmail.com>
2023-06-25 22:12:51 -0300

    Started with game logic, just defined the initial variables and functions.  Pending everything else

--- index.js
+++ index.js
@@ -1,66 +1,99 @@
+let game_status = "off";
+let sequence = [];
+let player_sequence_counter = 0;
+let button= {
+    green: "green",
+    red: "red",
+    yellow: "yellow",
+    blue: "blue"
+};
 
-$(".green-button").on("mousedown", function(e) {
+for (let i = 0; i < 20; i++) {
+    sequence.push(Math.floor(Math.random() * 4));
+}
+
+$(".green-button").on("mousedown", function() {
     $(this).addClass("green-button-active");
-}).on("mouseup", function(e) {
+    game_logic(button.green);
+}).on("touchstart", function() {
+    $(this).addClass("green-button-active");
+    game_logic(button.green);
+}).on("mouseup", function() {
     $(this).removeClass("green-button-active");
-}).on(".button").on("touchstart", function(e) {
-    $(this).addClass("green-button-active");
-}).on("touchend", function(e) {
+}).on("touchend", function() {
     $(this).removeClass("green-button-active green-button-hover");
 });
 
-$(".red-button").on("mousedown", function(e) {
+$(".red-button").on("mousedown", function() {
     $(this).addClass("red-button-active");
-}).on("mouseup", function(e) {
+    game_logic(button.red);
+}).on("touchstart", function() {
+    $(this).addClass("red-button-active");
+    game_logic(button.red);
+}).on("mouseup", function() {
     $(this).removeClass("red-button-active");
-}).on(".button").on("touchstart", function(e) {
-    $(this).addClass("red-button-active");
-}).on("touchend", function(e) {
+}).on("touchend", function() {
     $(this).removeClass("red-button-active red-button-hover");
 });
 
-$(".yellow-button").on("mousedown", function(e) {
+$(".yellow-button").on("mousedown", function() {
     $(this).addClass("yellow-button-active");
-}).on("mouseup", function(e) {
+    game_logic(button.yellow);
+}).on("touchstart", function() {
+    $(this).addClass("yellow-button-active");
+    game_logic(button.yellow);
+}).on("mouseup", function() {
     $(this).removeClass("yellow-button-active");
-}).on(".button").on("touchstart", function(e) {
-    $(this).addClass("yellow-button-active");
-}).on("touchend", function(e) {
+}).on("touchend", function() {
     $(this).removeClass("yellow-button-active yellow-button-hover");
 });
 
-$(".blue-button").on("mousedown", function(e) {
+$(".blue-button").on("mousedown", function() {
     $(this).addClass("blue-button-active");
-}).on("mouseup", function(e) {
+    game_logic(button.blue);
+}).on("touchstart", function() {
+    $(this).addClass("blue-button-active");
+    game_logic(button.blue);
+}).on("mouseup", function() {
     $(this).removeClass("blue-button-active");
-}).on(".button").on("touchstart", function(e) {
-    $(this).addClass("blue-button-active");
-}).on("touchend", function(e) {
+}).on("touchend", function() {
     $(this).removeClass("blue-button-active blue-button-hover");
 });
 
 if ($(window).width() > 768) {
-    $(".green-button").on("mouseover", function(e) {
+    $(".green-button").on("mouseover", function() {
         $(this).addClass("green-button-hover");
-    }).on("mouseout", function(e) {
+    }).on("mouseout", function() {
         $(this).removeClass("green-button-hover");
     });
 
-    $(".red-button").on("mouseover", function(e) {
+    $(".red-button").on("mouseover", function() {
         $(this).addClass("red-button-hover");
-    }).on("mouseout", function(e) {
+    }).on("mouseout", function() {
         $(this).removeClass("red-button-hover");
     });
 
-    $(".yellow-button").on("mouseover", function(e) {
+    $(".yellow-button").on("mouseover", function() {
         $(this).addClass("yellow-button-hover");
-    }).on("mouseout", function(e) {
+    }).on("mouseout", function() {
         $(this).removeClass("yellow-button-hover");
     });
 
-    $(".blue-button").on("mouseover", function(e) {
+    $(".blue-button").on("mouseover", function() {
         $(this).addClass("blue-button-hover");
-    }).on("mouseout", function(e) {
+    }).on("mouseout", function() {
         $(this).removeClass("blue-button-hover");
     });
+}
+
+function validate_sequence (input) {
+    if (input === sequence[player_sequence_counter]) {
+        player_sequence_counter++;
+        return true;
+    } else {
+        return false;
+    }
+}
+function game_logic (button_pressed) {
+
 }
